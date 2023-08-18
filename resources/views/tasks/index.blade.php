604a91248195dcf56ea2f4fa04ca849126c32717
Rasmus Schapiro <rasmus.schapiro@gmail.com>
2023-08-18 09:11:55 +0200

    added applied the bootstrap in index.blade.php

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="/bootstrap-5.3.1-dist/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
     <h1>Tasks</h1>
@@ -44,5 +45,7 @@
             @endif
         </table>
     </div>
+
+    <script src="/bootstrap-5.3.1-dist/js/bootstrap.js"></script>
 </body>
 </html>
