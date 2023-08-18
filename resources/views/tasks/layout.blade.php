f325877aa98519fe6600980f400e5dcc010875c4
Rasmus Schapiro <rasmus.schapiro@gmail.com>
2023-08-18 12:01:24 +0200

    deleted old views

--- resources/views/tasks/layout.blade.php
+++ resources/views/tasks/layout.blade.php
@@ -15,26 +15,3 @@
     
 </body>
 </html>
-
-
-
-
-
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel CRUD Application</title>
-    <link rel="stylesheet" href="/bootstrap-5.3.1-dist/css/bootstrap.css">
-</head>
-<body>
-<h1>Laravel CRUD Application</h1>
-<div class="container">
-    <br>
-    @yield('content')
-</div>
-    
-</body>
-</html> --}}
