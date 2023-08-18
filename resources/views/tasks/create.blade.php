8d2f9d240352e22b16b1692e725fe993f9ff201a
Rasmus Schapiro <rasmus.schapiro@gmail.com>
2023-08-18 11:32:05 +0200

    made new create view with bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,7 +2,51 @@
 
 @section('content')
 
-<h1>Create Task...</h1>
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New Task</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('task.index') }}">Back</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form method="POST" action="{{ route('task.store') }}">
+    @csrf
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Title:</strong>
+                <input type="text" name="title" class="form-control" placeholder="Title" value="{{ old('title') }}">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Description:</strong>
+                {{-- <input type="text" name="description" class="form-control" placeholder="Description" value="{{ old('description') }}"> --}}
+                <textarea type="text" name="description" class="form-control" placeholder="Description" style="height:150px"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Create Task</button>
+        </div>
+    </div>
+</form>
+
 
 @endsection
 
--- resources/views/tasks/layout.blade.php
+++ resources/views/tasks/layout.blade.php
@@ -1,6 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>Laravel CRUD Application</title>
+    <link rel="stylesheet" href="/bootstrap-5.3.1-dist/css/bootstrap.css">
+</head>
+<body>
+
+<h1>Laravel CRUD Application</h1>
+
+<div class="container">
+    <br>
+    @yield('content')
+</div>
+    
+</body>
+</html>
+
+
+
+
+
+{{-- <!DOCTYPE html>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -11,8 +33,8 @@
 <h1>Laravel CRUD Application</h1>
 <div class="container">
     <br>
-    @@yield('content')
+    @yield('content')
 </div>
     
 </body>
-</html>
+</html> --}}
