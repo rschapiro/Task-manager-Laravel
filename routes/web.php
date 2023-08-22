96bbda7d72c03e61b93e775bb5fdccb9a0f0cfea
Rasmus Schapiro <rasmus.schapiro@gmail.com>
2023-08-22 13:02:16 +0200

    setting up a welcome screen, authentication and routing to task list

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,13 +24,15 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
-Route::prefix('tasks')->group(function(){
-    Route::get('list', [TaskController::class, 'index'])->name('task.index');
-    Route::get('create', [TaskController::class, 'create'])->name('task.create');
-    Route::post('save', [TaskController::class, 'store'])->name('task.store');
-    // Route::get('{id}', [TaskController::class], 'show')->name('task.show');
-    Route::get('edit/{id}', [TaskController::class, 'edit'])->name('task.edit');
-    Route::post('update/{id}', [TaskController::class, 'update'])->name('task.update');
-    Route::post('{task}/destroy', [TaskController::class, 'destroy'])->name('task.destroy');
-    Route::post('complete-task/{id}', [TaskController::class, 'setStatus'])->name('task.complete');
-});
+Route::resource('tasks/', TaskController::class)->middleware(['auth']);
+
+// Route::prefix('tasks')->group(function(){
+//     Route::get('list', [TaskController::class, 'index'])->name('task.index');
+//     Route::get('create', [TaskController::class, 'create'])->name('task.create');
+//     Route::post('save', [TaskController::class, 'store'])->name('task.store');
+//     // Route::get('{id}', [TaskController::class], 'show')->name('task.show');
+//     Route::get('edit/{id}', [TaskController::class, 'edit'])->name('task.edit');
+//     Route::post('update/{id}', [TaskController::class, 'update'])->name('task.update');
+//     Route::post('{task}/destroy', [TaskController::class, 'destroy'])->name('task.destroy');
+//     Route::post('complete-task/{id}', [TaskController::class, 'setStatus'])->name('task.complete');
+// });
